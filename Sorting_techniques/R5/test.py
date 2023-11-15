67c1c743c95e57eae6f9bec2630266234dfbde95
durga256 <durgadevi256@gmail.com>
2023-11-14 23:04:18 -0500

    More DSA

--- Sorting_techniques/R5/test.py
+++ Sorting_techniques/R5/test.py
@@ -1,25 +1,31 @@
-arr = [10, 7, 8, 9, 1, 5]
+arr = [12, 11, 13, 5, 6, 7]
 
-def partition(arr, low, high):
-    pivot = arr[high]
+def heapify(arr, i, n):
+    largest = i
+    left_child = 2*i + 1
+    right_child = 2*i + 2
 
-    j = low
-    while j <= high:
-        if arr[j] < pivot:
-            arr[low], arr[j] = arr[j], arr[low]
-            low += 1
-        elif arr[j] > pivot:
-            arr[high], arr[j] = arr[j], arr[high]
-            high -= 1; j-= 1
-        j += 1
-    print(arr)
-    return low
+    if left_child < n and arr[largest] < arr[left_child]:
+        largest = left_child
+    if right_child < n and arr[largest] < arr[right_child]:
+        largest = right_child
 
-def f(arr,low,high):
-    if low < high:
-        pi = partition(arr, low, high)
-        f(arr, low, pi-1)
-        f(arr, pi+1, high)
+    if largest != i:
+        arr[largest], arr[i] = arr[i], arr[largest]
+        heapify(arr, largest, n)
 
-f(arr,0,len(arr)-1)
-print(arr)
+def heap_sort():
+    n = len(arr)
+
+    for i in range(n//2-1, -1,-1):
+        heapify(arr, i, n)
+        print(arr)
+
+    for i in range(n-1,0,-1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, 0, i)
+        #print(arr)
+
+    #print(arr)
+
+heap_sort()
